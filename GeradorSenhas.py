eecda66436956c8d194dd99699b129589ec787d7
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2023-05-24 16:54:41 -0300

    Versão limpa

--- GeradorSenhas.py
+++ GeradorSenhas.py
@@ -4,11 +4,8 @@ lowerCase = "abcdefghijklmnopqrstuvwxyz"
 upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "0123456789"
 symbols = "!@#$%&*()"
-#==============Caracteres====================
 line = lowerCase + upperCase + numbers + symbols
-
+#==============Função====================
 tamanhoSenha = int(input("Defina o tamanho da senha: "))
-
 passWord = "".join(random.sample(line, tamanhoSenha))
-
 print("senha gerada: {}".format(passWord))
